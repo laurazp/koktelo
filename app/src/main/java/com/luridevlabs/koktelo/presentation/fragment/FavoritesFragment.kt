e2b0a843eeef3e64a583d5f6a8640e79a8b7549e
laurazp <laurikazp@gmail.com>
2023-09-22 02:11:31 +0200

    Home, Cocktails and Favorites Fragments added

---
+++ app/src/main/java/com/luridevlabs/koktelo/presentation/fragment/FavoritesFragment.kt
@@ -0,0 +1,25 @@
+package com.luridevlabs.koktelo.presentation.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.luridevlabs.koktelo.R
+import com.luridevlabs.koktelo.databinding.FragmentCocktailsBinding
+import com.luridevlabs.koktelo.databinding.FragmentFavoritesBinding
+
+class FavoritesFragment : Fragment() {
+
+    private val binding: FragmentFavoritesBinding by lazy {
+        FragmentFavoritesBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/luridevlabs/koktelo/presentation/fragment/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.luridevlabs.koktelo.presentation.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.luridevlabs.koktelo.R
+import com.luridevlabs.koktelo.databinding.FragmentCocktailsBinding
+import com.luridevlabs.koktelo.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private val binding: FragmentHomeBinding by lazy {
+        FragmentHomeBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+}
